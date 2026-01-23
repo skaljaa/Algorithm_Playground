96f561f8425921eb32109d3dbf5a205a13b63c85
skaljaa <tarikskaljic01@gmail.com>
2026-01-23 08:56:16 +0100

    BST edit

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -8,7 +8,7 @@ class BST:
     def __init__(self):
         self.root = None
     
-    def insert(self, x):
+    def insert(self, x: int) -> None:
         
         node = Node(x)
         
@@ -29,7 +29,7 @@ class BST:
                     return
                 current = current.right
             
-    def search(self,x):
+    def search(self,x: int) -> bool:
         if self.root is not None:
             current = self.root
             while current is not None:
@@ -42,37 +42,38 @@ class BST:
             return False
         return False
     
-    def _inorder_helper(self,node,result):
+    def _inorder_helper(self,node,result)-> None:
         if node is None:
             return
-        self._inorder_helper(node.left,result)
-        result.append(node.data)
-        self._inorder_helper(node.right,result)
-    
-    def inorder(self):
-        result = []
-        self._inorder_helper(self.root,result)
-        return result
-    def preorder(self):
-        result = []
-        self._preorder_helper(self.root, result)
-        return result
-
-    def _preorder_helper(self, node, result):
+        self._inorder_helper(node.left,result) #left
+        result.append(node.data) # root
+        self._inorder_helper(node.right,result) # right
+        
+    def _postorder_helper(self, node, result) -> None:
+        if node is None:
+            return
+        self._postorder_helper(node.left, result)   # Left first
+        self._postorder_helper(node.right, result)  # Then right
+        result.append(node.data) # root last
+        
+    def _preorder_helper(self, node, result) -> None:
         if node is None:
             return
         result.append(node.data)                    # Root first
         self._preorder_helper(node.left, result)    # Then left
         self._preorder_helper(node.right, result)   # Then right
+    
+    def inorder(self) -> list:
+        result = []
+        self._inorder_helper(self.root,result)
+        return result
+    def preorder(self) -> list:
+        result = []
+        self._preorder_helper(self.root, result)
+        return result
         
-    def postorder(self):
+    def postorder(self) -> list:
         result = []
         self._postorder_helper(self.root, result)
         return result
 
-    def _postorder_helper(self, node, result):
-        if node is None:
-            return
-        self._postorder_helper(node.left, result)   # Left first
-        self._postorder_helper(node.right, result)  # Then right
-        result.append(node.data) 
